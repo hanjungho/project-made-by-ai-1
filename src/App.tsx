a5346ff98dfb3f11e0a66b3e157a9bb2df8b6ce1
hanjungho <thegamerhan@gmail.com>
2025-06-15 01:50:55 +0900

    fix: Remove unused React import to resolve TS6133 warning

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { useAuthStore } from './store/authStore';
 import { VERSION, BUILD_TIME } from './version';
